ed16d1899daa370dd49f77b7ec679d1142cd4af8
Vinicius Imhof Waldrigues <vinicius.waldrigues@unifebe.edu.br>
2025-12-12 15:02:52 -0300

    Criado sessão com prompts para cadastro de usuário em hospital

--- assets/js/prompt.js
+++ assets/js/prompt.js
@@ -15,14 +15,73 @@ function solicitarInt(){
 }
 
 function solicitarFloat(){
-    let saldoBancario = parseFloat(prompt("Digite o saldo bancário"))
+    let saldoBancario = parseFloat(prompt("Digite o saldo bancário"));
+    const saque = parseFloat(prompt("Digite o valor do saque"));
+    
+    saldoBancario = saldoBancario - saque;
+
+    alert("Saque realizado: " + saque + "\nSaldo atual: " + saldoBancario);
 
 }
 
 function solicitarBoolean(){
+    let vencido = confirm("Produto vencido?");
+
+    alert("Produto vencido: " + vencido)
 
 }
 
 function paciente(){
+    let nome = prompt("Digite o nome do paciente");
+    const idade = parseInt(prompt("Digite a idade"));
+    const peso = parseFloat(prompt("Digite o peso"));
+    const possuiRg = confirm("Possui rg?");
+    const possuiPlanoSaude = confirm("Possui plano de saúde?");
+    const cpf = prompt("Digite seu CPF");
+
+    let planoSaude = "";
+    // se possui plano de saude for igual a verdadeiro
+    if (possuiPlanoSaude === true) {
+        planoSaude = prompt("Digite o nome do plano de saúde");
+    }
+
+    const tipoSanguineo = prompt("Digite o tipo sanguíneo");
+    const altura = parseFloat(prompt("Digite sua altura"));
+    const possuiAlergia = confirm("Possuí alergia?");
+
+    const escalaDaDor = parseInt(prompt("Escala de dor: [0...10]"))
+
+    const urgencia = prompt("Escolha o nível de Urgência [Migué, Atestado, Dorzinha, Dengue, Dor Extrema]")
+
+    let tempoEspera = 0;
+    if (urgencia === "Migué"){
+        tempoEspera = 180;
+    } else if (urgencia === "Atestado"){
+        tempoEspera = 150;
+    } else if (urgencia === "Dorzinha"){
+        tempoEspera = 120;
+    } else if (urgencia === "Dengue") {
+        tempoEspera = 60;
+    } else if (urgencia === "Dor Extrema") {
+        tempoEspera = 30;
+    } else {
+        alert("Nível de urgência inexistente");
+        return;
+    }
+
+    alert(
+        "Nome: " + nome +
+        "\nIdade: " + idade +
+        "\nPeso: " + peso + 
+        "\nPossui RG: " +    possuiRg +
+        "\nCPF: " + cpf +
+        "\nPossui plano de saúde: " + (possuiPlanoSaude ? "Sim" : "Não") +
+        (possuiPlanoSaude ? "\nPlano de Saúde: " + planoSaude + "" : "") +
+        "\nTipo sanguíneo: " + tipoSanguineo +
+        "\nAltura: " + altura + "m" +
+        "\nNível de urgência: " + urgencia + 
+        "\n Nível de urgência: " + tempoEspera +
+        "\nTempo de espera: " + tempoEspera
+    );
 
 }
